b25ab82079aabce6b25e86dab7f877190a61c746
Paul Burakov <pburakov@spotify.com>
2019-02-13 12:42:36 +0000

    Add comments and some improvements on code clarity

--- file/reader.go
+++ file/reader.go
@@ -3,6 +3,8 @@ package file
 import "time"
 
 type Reader interface {
-	ReadLine() (ts time.Time, data []byte, err error)
+	// ReadLine reads the next line from the input file, extracts the timestamp
+	// value and returns it with the binary data from the input line.
+	ReadLine() (ts time.Time, data []byte, e error)
 	// TODO: add PeekNextTS() method
 }
